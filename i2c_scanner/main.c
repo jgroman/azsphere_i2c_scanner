63ce12d25c9fd60486699b37e53c5388372e7e80
jgroman <jgroman@users.noreply.github.com>
2019-07-24 14:57:20 +0200

    Initial project files

---
+++ i2c_scanner/main.c
@@ -0,0 +1,105 @@
+﻿#include <errno.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+// applibs_versions.h defines the API struct versions to use for applibs APIs.
+#include "applibs_versions.h"
+#include <applibs/log.h>
+#include <applibs/i2c.h>
+
+// Import project hardware abstraction from project property "Target Hardware Definition Directory"
+#include <hw/project_hardware.h>
+
+// Support functions.
+static void TerminationHandler(int signalNumber);
+static int InitPeripheralsAndHandlers(void);
+static void ClosePeripheralsAndHandlers(void);
+
+// File descriptors - initialized to invalid value
+static int i2cFd = -1;
+
+// Termination state
+static volatile sig_atomic_t terminationRequired = false;
+
+/// <summary>
+///     Signal handler for termination requests. This handler must be async-signal-safe.
+/// </summary>
+static void TerminationHandler(int signalNumber)
+{
+	// Don't use Log_Debug here, as it is not guaranteed to be async-signal-safe.
+	terminationRequired = true;
+}
+
+
+/// <summary>
+///     Set up SIGTERM termination handler, initialize peripherals, and set up event handlers.
+/// </summary>
+/// <returns>0 on success, or -1 on failure</returns>
+static int InitPeripheralsAndHandlers(void)
+{
+	struct sigaction action;
+	memset(&action, 0, sizeof(struct sigaction));
+	action.sa_handler = TerminationHandler;
+	sigaction(SIGTERM, &action, NULL);
+
+	// Init I2C
+	i2cFd = I2CMaster_Open(PROJECT_ISU2_I2C);
+	if (i2cFd < 0) {
+		Log_Debug("ERROR: I2CMaster_Open: errno=%d (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+	int result = I2CMaster_SetBusSpeed(i2cFd, I2C_BUS_SPEED_STANDARD);
+	if (result != 0) {
+		Log_Debug("ERROR: I2CMaster_SetBusSpeed: errno=%d (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+	result = I2CMaster_SetTimeout(i2cFd, 100);
+	if (result != 0) {
+		Log_Debug("ERROR: I2CMaster_SetTimeout: errno=%d (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+		return 0;
+}
+
+/// <summary>
+///     Close peripherals and handlers.
+/// </summary>
+static void ClosePeripheralsAndHandlers(void)
+{
+	close(i2cFd);
+}
+
+/// <summary>
+///     Main entry point for this application.
+/// </summary>
+int main(int argc, char *argv[])
+{
+    Log_Debug("\n*** I2C Scan Starting ***\n");
+
+	if (InitPeripheralsAndHandlers() != 0) {
+		terminationRequired = true;
+	}
+
+	if (!terminationRequired) {
+		I2C_DeviceAddress address;
+		ssize_t bytes;
+		uint8_t reply;
+
+		for (address = 1; address < 127; address++)
+		{
+			bytes = I2CMaster_Read(i2cFd, address, &reply, 1);
+			if (bytes >= 0) {
+				Log_Debug("Detected device at 0x%02x\n", address);
+			}
+		}
+	}
+
+	ClosePeripheralsAndHandlers();
+	return 0;
+}
